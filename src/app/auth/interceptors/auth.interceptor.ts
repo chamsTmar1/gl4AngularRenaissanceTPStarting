81b12465914712fe2273f3ccf91185413cd54bfd
Donia Bougrine <124002250+doniab7@users.noreply.github.com>
2024-11-16 10:28:35 +0100

    fix interceptor

--- src/app/auth/interceptors/auth.interceptor.ts
+++ src/app/auth/interceptors/auth.interceptor.ts
@@ -1,11 +1,11 @@
 import { inject } from '@angular/core';
-import {HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
 import { CONSTANTES } from 'src/config/const.config';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  
+
   if (authService.authenticated()) {
     const token = localStorage.getItem(CONSTANTES.tokenKey) ?? '';
     const clonedRequest = req.clone({
