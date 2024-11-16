f14401ecfd1d15d6ecc95e4fb7982659f6b55f15
Donia Bougrine <124002250+doniab7@users.noreply.github.com>
2024-11-16 12:16:44 +0100

    fix constants

--- src/config/const.config.ts
+++ src/config/const.config.ts
@@ -1,19 +1,17 @@
+import { TodoStatus } from 'src/app/todo/model/todo';
 
-import { TodoStatus } from "src/app/todo/model/todo";
-
-export const CONSTANTES:{
-    waiting:TodoStatus;
-    tokenKey: 'token';
-    userData: 'userData';
-    inProgress: TodoStatus;
-    done: TodoStatus;
-    defaultImage:string;
-
-}={
-   waiting:'waiting',
-   inProgress:'inprogress',
-   done : 'done',
-   defaultImage: 'cv.png',
-
-
-  };
+export const CONSTANTES: {
+  waiting: TodoStatus;
+  tokenKey: string;
+  userData: string;
+  inProgress: TodoStatus;
+  done: TodoStatus;
+  defaultImage: string;
+} = {
+  waiting: 'waiting',
+  inProgress: 'inprogress',
+  done: 'done',
+  defaultImage: 'cv.png',
+  tokenKey: 'token',
+  userData: 'userData',
+};
