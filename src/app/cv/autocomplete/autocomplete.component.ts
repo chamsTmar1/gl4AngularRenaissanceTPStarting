072c3a25d389d64889bd303d3e471f8ddc7d7f06
GH1hatem <gharsallahhatem58@gmail.com>
2024-12-03 19:22:41 +0100

    autocomplete

--- src/app/cv/autocomplete/autocomplete.component.ts
+++ src/app/cv/autocomplete/autocomplete.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from "@angular/core";
 import { FormBuilder, AbstractControl } from "@angular/forms";
-import { debounceTime, distinctUntilChanged, switchMap, tap } from "rxjs";
+import { catchError, debounceTime, distinctUntilChanged, of, switchMap, tap } from "rxjs";
 import { CvService } from "../services/cv.service";
 
 @Component({
@@ -15,4 +15,13 @@ export class AutocompleteComponent {
     return this.form.get("search")!;
   }
   form = this.formBuilder.group({ search: [""] });
+  filteredCvs$ = this.search.valueChanges.pipe(
+    debounceTime(400),
+    distinctUntilChanged(),
+    switchMap((name) =>
+      name
+        ? this.cvService.selectByName(name).pipe(catchError(() => of([])))
+        : of([])
+    )
+  );
 }
