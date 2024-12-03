fc72a65ed4300fe3fc483be4b28b4f9e3e5313e3
Zeineb Ben Abdallah <zeineb.benabdallah@insat.ucar.tn>
2024-12-03 21:52:22 +0100

    lammen

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,29 +1,36 @@
-import { Component } from "@angular/core";
-import { BehaviorSubject, Observable, concatMap, map, scan, takeWhile,tap } from "rxjs";
-import { Product } from "./dto/product.dto";
-import { ProductService } from "./services/product.service";
-import { Settings } from "./dto/product-settings.dto";
-import { ProductApiResponse } from "./dto/product-api-response.dto";
+import { Component } from '@angular/core';
+import {
+  BehaviorSubject,
+  Observable,
+  concatMap,
+  map,
+  scan,
+  takeWhile,
+} from 'rxjs';
+import { Product } from './dto/product.dto';
+import { ProductService } from './services/product.service';
+import { Settings } from './dto/product-settings.dto';
+import { ProductApiResponse } from './dto/product-api-response.dto';
 
 @Component({
-  selector: "app-products",
-  templateUrl: "./products.component.html",
-  styleUrls: ["./products.component.css"],
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
   products$!: Observable<Product[]>;
   private loadMore$ = new BehaviorSubject<Settings>({ limit: 12, skip: 0 });
-  private totalProducts: number = 0;  
+  private totalProducts: number = 0;
 
   constructor(private productService: ProductService) {
     this.products$ = this.loadMore$.pipe(
       concatMap((settings) =>
-        this.productService.getProducts(settings).pipe(//pourquoi quand j'utilise mergeMap  le takewhile n'a aucun effet 
+        this.productService.getProducts(settings).pipe(
           map((response: ProductApiResponse) => {
             if (this.totalProducts === 0) {
               this.totalProducts = response.total;
             }
-           return {
+            return {
               products: response.products,
             };
           })
@@ -35,22 +42,17 @@ export class ProductsComponent {
         }),
         { products: [] as Product[] }
       ),
-      map((accumaltedlist) => accumaltedlist.products),  //ml5r map bch ta5ou reuslt ili jaya ml scan w bch traj3 observable fih list of products accumated
-      /* role of true : When set to true the value that caused predicate to return false will also be emitted.*/
-
-      /* Quand tu ajoutes true, la dernière valeur qui fait que la condition devient fausse est incluse dans l'émission.
-       Cela garantit que la liste finale des produits (ayant une longueur 194 de dans notre exemple) 
-       est émise avant que le flux ne se termine.*/ 
-      takeWhile( (products) => products.length < this.totalProducts,true)
-      //ne sera pas realise
-      )
-      
-    ;
+      map((accumaltedlist) => accumaltedlist.products),
+      takeWhile((products) => products.length < this.totalProducts, true)
+    );
   }
 
   loadMore() {
     const currentSkip = this.loadMore$.value.skip;
     const currentLimit = this.loadMore$.value.limit;
-    this.loadMore$.next({ limit: currentLimit, skip: currentSkip + currentLimit });
+    this.loadMore$.next({
+      limit: currentLimit,
+      skip: currentSkip + currentLimit,
+    });
   }
 }
